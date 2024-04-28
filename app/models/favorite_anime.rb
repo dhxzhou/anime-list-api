124c5b2d9ba49759cc9da4af76a93eb18c7c1206
dhxzhou <dzhou03@gmail.com>
2024-04-28 09:54:08 -0700

    create user authentication

--- app/models/favorite_anime.rb
+++ app/models/favorite_anime.rb
@@ -1,2 +1,4 @@
 class FavoriteAnime < ApplicationRecord
+  belongs_to :user
+  belongs_to :anime
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(name: "David", email: "david@eample.com", password_digest: "password", image_url: "example.jpg")
+
+Anime.create!(name: "One Piece", image_url: "Onepiece.jpg", description: "fill out later", genre: "adventure")
+Anime.create!(name: "Naruto", image_url: "Naruto.jpg", description: "fill out later", genre: "fantasy")
+Anime.create!(name: "Bleach", image_url: "Bleach.jpg", description: "fill out later", genre: "adventure")
+
+FavoriteAnime.create!(user_id: 1, anime_id: 1, finished: true)
+FavoriteAnime.create!(user_id: 1, anime_id: 2, finished: true)
+FavoriteAnime.create!(user_id: 1, anime_id: 3, finished: true)
+
+Manga.create!(name: "One Piece", image_url: "Onepiece.jpg", description: "fill out later", genre: "adventure")
+Manga.create!(name: "Naruto", image_url: "Naruto.jpg", description: "fill out later", genre: "fantasy")
+Manga.create!(name: "Bleach", image_url: "Bleach.jpg", description: "fill out later", genre: "adventure")
+
+FavoriteManga.create!(user_id: 1, manga_id: 1, finished: true)
+FavoriteManga.create!(user_id: 1, manga_id: 2, finished: true)
+FavoriteManga.create!(user_id: 1, manga_id: 3, finished: true)
